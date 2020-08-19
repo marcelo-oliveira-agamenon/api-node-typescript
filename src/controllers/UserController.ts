c2f2e8aa853e76597ab14a01e065a6c7097ce0ef
Marcelo Oliveira <marcelo_alfahot@hotmail.com>
2020-08-19 20:13:28 -0300

    DB connection

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express'
 
 class UserController {
+  public connection
+
   public async list (req: Request, res: Response): Promise<Response> {
     return res.send('dasas')
   }
